1a0d7cf1b9cd640a854d02fb545aede57f4c8ea1
aboubakrNdong <47300496+aboubakrNdong@users.noreply.github.com>
2025-09-10 10:10:28 +0200

    Delete codeForGithub directory

--- codeForGithub/main/java/com/cl/msipen03/entities/ExcelGenerationEntity.java
+++
@@ -1,5 +0,0 @@
-package com.cl.msipen03.entities;
-
-import java.io.ByteArrayInputStream;
-
-public record ExcelGenerationEntity(ByteArrayInputStream contentOfExcelFile, Integer idReper) {}
--- codeForGithub/main/java/com/cl/msipen03/service/RemittanceBuildExcelService.java
+++
@@ -1,8 +0,0 @@
-package com.cl.msipen03.service;
-
-import com.cl.msipen03.entities.RemittanceRequestPaginationResponse;
-import java.io.ByteArrayInputStream;
-
-public interface RemittanceBuildExcelService {
-    ByteArrayInputStream generateExcelContent(RemittanceRequestPaginationResponse response);
-}
